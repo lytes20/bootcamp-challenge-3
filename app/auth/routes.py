8f8468c7dad53ec6e13472f0fe16b3bf269bc3ea
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2018-06-05 18:48:41 +0300

    create endpoint for login

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -16,11 +16,27 @@ class RegisterUser(MethodView):
             isAdmin = post_data.get('isAdmin')
             
             new_user = User(username, email, password, isAdmin)
-            return jsonify({"new_user":new_user.__dict__}), 201
+            return jsonify({"new_user":new_user.__dict__}), 200
+
+class LoginUser(MethodView):
+    """ class to login a user """    
+    def post(self):
+        """ Login a user """
+        if request.content_type == 'application/json':
+            post_data = request.get_json()
+            email = post_data.get('email')
+            password = post_data.get('password')
+            
+            if email != 'email@email.com' or password != 'secret':
+                return jsonify({"msg": "Invalid username or password"}), 401
+            return jsonify({"msg":"Successful login"}), 200
+
            
 
 
 
 registration_view = RegisterUser.as_view('register_user')
+login_view = LoginUser.as_view('login')
 
 auth.add_url_rule("/api/v1/user/register",  view_func=registration_view, methods=['POST'])
+auth.add_url_rule('/api/v1/user/login', view_func=login_view, methods=['POST'])
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -16,5 +16,23 @@ class AuthTest(TestCase):
                 content_type='application/json',
                 data=json.dumps(dict(username="Gideon B", email="email@email.com", password="secret"))
                 )            
-            reply = json.loads(response.data)            
-            self.assertEqual(response.status_code, 201)
+            self.assertEqual(response.status_code, 200)
+
+
+    def test_user_login_successfully(self):
+        """ Test for login end point """
+        with self.client:
+            res = self.client.post(
+                "api/v1/user/register",
+                content_type='application/json',
+                data=json.dumps(dict(username="Gideon B", email="email@email.com", password="secret"))
+                )
+            self.assertEqual(res.status_code, 200)
+            
+            response = self.client.post(
+                "api/v1/user/login",
+                content_type='application/json',
+                data=json.dumps(dict(email="email@email.com", password="secret"))
+                )            
+            
+            self.assertEquals(response.status_code, 200)
