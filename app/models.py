8ef4655cdb991574d9e89a9db5a2bf98837fc369
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2018-06-06 20:14:48 +0300

    create a requests model

--- app/models.py
+++ app/models.py
@@ -11,3 +11,13 @@ class User:
         self.email= email
         self.password = password
         self.isAdmin = isAdmin
+
+
+class MaintenanceRequest:
+    def __init__(self, title, desc, requester_name, request_status, request_id):
+        """Initialise the MaintenanceRequest"""
+        self.title = title
+        self.description = desc
+        self.requester_name = requester_name
+        self.request_status = request_status
+        self.request_id = request_id
